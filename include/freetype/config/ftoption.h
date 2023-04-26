4486138b22ad6cc3734e191d9a7e83d8da5e13f5
Gabriel Cuvillier <contact@gabrielcuvillier.pro>
2023-04-26 15:02:24 +0200

    restored some options for TT

--- include/freetype/config/ftoption.h
+++ include/freetype/config/ftoption.h
@@ -183,7 +183,7 @@ FT_BEGIN_HEADER
  *
  *   Define this macro if you want to enable this 'feature'.
  */
-/* #define FT_CONFIG_OPTION_USE_LZW */
+#define FT_CONFIG_OPTION_USE_LZW
 
 
 /**************************************************************************
@@ -223,7 +223,7 @@ FT_BEGIN_HEADER
  *   options set by those programs have precedence, overwriting the value
  *   here with the configured one.
  */
-/* #define FT_CONFIG_OPTION_SYSTEM_ZLIB */
+#define FT_CONFIG_OPTION_SYSTEM_ZLIB
 
 
 /**************************************************************************
@@ -272,7 +272,7 @@ FT_BEGIN_HEADER
  *   options set by those programs have precedence, overwriting the value
  *   here with the configured one.
  */
-/* #define FT_CONFIG_OPTION_USE_PNG */
+#define FT_CONFIG_OPTION_USE_PNG
 
 
 /**************************************************************************
@@ -537,7 +537,7 @@ FT_BEGIN_HEADER
  * embedded bitmaps in all formats using the 'sfnt' module (namely
  * TrueType~& OpenType).
  */
-/* #define TT_CONFIG_OPTION_EMBEDDED_BITMAPS */
+#define TT_CONFIG_OPTION_EMBEDDED_BITMAPS
 
 
 /**************************************************************************
@@ -615,7 +615,7 @@ FT_BEGIN_HEADER
  * Do not `#undef` this macro here, since the build system might define it
  * for certain configurations only.
  */
-/* #define TT_CONFIG_OPTION_BYTECODE_INTERPRETER */
+#define TT_CONFIG_OPTION_BYTECODE_INTERPRETER
 
 
 /**************************************************************************
